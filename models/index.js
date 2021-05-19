f6f452eebdde4b8fce2873e60a230c62cfab4bee
Andy Schachter <andy.schachter@gmail.com>
2021-05-18 20:34:09 -0400

    added SQLize to Manufacturers.js and models index.js

---
+++ models/index.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize')
+const ProductsModel = require('./products')
+const ManufacturersModel = require('./Manufacturers')
+
+const connection = new Sequelize('candies', 'candies', 'C4nd13$!', {
+  host: 'localhost', dialect: 'mysql'
+})
+
+const Products = ProductsModel(connection, Sequelize)
+const Manufacturers = ManufacturersModel(connection, Sequelize)
+
+module.exports = {
+  Products,
+  Manufacturers,
+}
+
